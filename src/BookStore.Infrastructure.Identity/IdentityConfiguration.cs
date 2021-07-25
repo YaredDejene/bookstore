70ffbc91aa7be257d12bc3d0a082ff43a81fda02
Yared Dejene <yareddej@gmail.com>
2021-07-25 23:05:01 +0200

    Identity Project Added

---
+++ src/BookStore.Infrastructure.Identity/IdentityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetDevPack.Identity;
+using NetDevPack.Identity.Jwt;
+
+namespace BookStore.Infrastructure.Identity
+{
+    public static class IdentityConfiguration
+    {
+        public static void AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentityEntityFrameworkContextConfiguration(options => 
+                options.UseNpgsql(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("BookStore.Infrastructure.Identity")));
+            
+
+            // Default Identity configuration from NetDevPack.Identity
+            services.AddIdentityConfiguration();
+
+            // JWT
+            services.AddJwtConfiguration(configuration, "AppSettings");
+        }
+    }
+}
